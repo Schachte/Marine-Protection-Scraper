67bd07fdf26871d6ae336bfbfc87d122dc9a6bc7
Ryan Schachte <code@ryan-schachte.com>
2021-06-21 19:10:22 -0700

    ocean update

--- CrawlTheOcean.js
+++ CrawlTheOcean.js
@@ -23,13 +23,13 @@ let countriesOfInterest = [
     "Anguilla (British overseas territory)",
     "British Virgin Islands (British overseas territory)",
     "Cayman Islands (British overseas territory)",
-    // "Montserrat",
+    "Montserrat",
     "Turks and Caicos Islands (British overseas territory)",
     "Aruba (Constituent country in the Kingdom of the Netherlands)",
     "Curaçao (Constituent country in the Kingdom of the Netherlands)",
     "Sint Maarten (Constituent country in the Kingdom of the Netherlands)",
-    // "Saba (Special municipality of the Netherlands)",
-    // "Sint Eustatius (Special municipality of the Netherlands)",
+    "Saba (Special municipality of the Netherlands)",
+    "Sint Eustatius (Special municipality of the Netherlands)",
     "Saint Barthélemy (Overseas collectivity of France)",
     "Saint Martin (Overseas collectivity of France)",
     "Guadeloupe (French overseas region)",
@@ -38,106 +38,4 @@ let countriesOfInterest = [
     "United States Virgin Islands (U.S. territory)"
 ]
 
-
-    const matchRelevantData = (country) => {
-        let relevancyFix = country.replace(/\([^\)]*\)/g, '')  
-            .match(/(\S+)/g)
-            .map((item) => item.trim())
-            .filter((item) => item != null)
-            .filter((item) => item.trim() != '')
-            .join("+");
-        return relevancyFix
-    }
-
-    const cleanseCountryName = (country) => {
-        let lowerCountry = country.toLowerCase();
-        return matchRelevantData(lowerCountry)
-    }
-
-    const cleanseAndGenerate = (countries, type) => {
-        let relevantUrls = []
-        for (let country in countries) {
-            const currentCountry = cleanseCountryName(countries[country])
-            if (type === "country") {
-                let cleansedCountry = countryUrl.replace("country_term", currentCountry)
-                relevantUrls.push(cleanseCountryName(cleansedCountry))
-            } else if (type === "region") {
-                let cleansedCountry = mpaUrl.replaceAll("country_term", currentCountry)
-                relevantUrls.push(cleanseCountryName(cleansedCountry))
-            }
-        }
-        return relevantUrls
-    }
-
-    function extractItems() {
-        let data = [];
-        let elements = document.getElementsByClassName('card--site');
-        for (var element of elements)
-            data.push(element.textContent);
-        return data
-    }
-
-    function parseResultCount() {
-        let data = [];
-        let elements = document.getElementsByClassName('search__results-bar');
-        for (var element of elements){
-            data.push(element.innerText.match(/\(([^)]+)\)/)[1])
-        }
-
-        return data
-    }
-
-    function loadCountryPage() {
-        document.getElementsByClassName('card--country')[0].click();
-        return []
-    }
-
-    function loadCountryPercentData() {
-        return document.getElementsByClassName('card__number-large')[1].innerText
-    }
-
-    async function scrapeInfiniteScroll(page) {
-        return await page.evaluate(parseResultCount)
-    }
-
-    async function extractRegionData() {
-        const browser = await puppeteer.launch({ headless: false })
-        const page = await browser.newPage()
-        page.setViewport({width: 1280, height: 926})
-        const regionsUrls = cleanseAndGenerate(countriesOfInterest, "region")
-
-        for (let region in regionsUrls) {
-            await page.goto(regionsUrls[region])
-            await page.waitForSelector('.card--site');
-
-            const items = await scrapeInfiniteScroll(
-                page,
-                extractItems
-            )
-            countryData[countriesOfInterest[region]] = {...countryData[countriesOfInterest[region]], mpaCount: items[0]}
-        }
-    }
-
-    async function extractCountryData() {
-        const browser = await puppeteer.launch({ headless: false })
-        const page = await browser.newPage()
-        page.on('console', consoleObj => console.log(consoleObj.text()));
-        page.setViewport({width: 1280, height: 926})
-
-        const countriesUrls = cleanseAndGenerate(countriesOfInterest, "country")
-        for (let country in countriesUrls) {
-            await page.goto(countriesUrls[country])
-            await page.waitForSelector('.card--country');
-            await page.evaluate(loadCountryPage)
-            await page.waitForSelector('.card__number-large');
-            const result = await page.evaluate(loadCountryPercentData)
-            countryData[countriesOfInterest[country]] = {...countryData[countriesOfInterest[country]], marinePercentCovered: result}
-        }
-    }
-
-    async function run() {
-        await extractRegionData()
-        await extractCountryData()
-        console.log(countryData)
-    }
-    run()
+console.log(countriesOfInterest.length)
